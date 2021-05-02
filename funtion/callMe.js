3eb4f02e96601ccd63c626f465c9921a83b5e301
avinashboy <52669990+avinashboy@users.noreply.github.com>
2021-05-02 14:32:16 +0530

    Delete funtion directory

--- funtion/callMe.js
+++
@@ -1,8 +0,0 @@
-const axios = require("axios")
-let moment = require('moment-timezone');
-
-async function makeGetRequest() {
-  await axios.get(`https://jsonserver8421.herokuapp.com/`).then(res => console.log(`${res.data} time is : ${moment().tz("Asia/Kolkata").format('lll')}`)).catch(err => console.log(err));
-}
-
-module.exports = makeGetRequest
