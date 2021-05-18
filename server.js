0e145fde1588feccc6d0d15b22203064753d3189
avinashboy <52669990+avinashboy@users.noreply.github.com>
2021-05-18 11:55:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,19 +5,19 @@ const express = require('express'),
   cors = require('cors'),
   helmet = require("helmet"),
   toobusy = require('toobusy-js'),
-  nodeSchedule = require('node-schedule')
+  nodeSchedule = require('node-schedule'),
+  cluster = require('cluster'),
+  os = require('os')
 
-const port = process.env.PORT || 3333
+const port = process.env.PORT || 3333, numCpu = os.cpus().length
+
 dotenv.config()
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(helmet())
-app.use(express.urlencoded({
-  extended: true,
-  limit: "1kb"
-}))
+app.use(express.urlencoded({ extended: true, limit: "1kb" }))
 app.use(function (req, res, next) {
   if (toobusy()) return res.status(503).send("Server Too Busy");
   next();
@@ -34,39 +34,6 @@ app.use("/brithCounter", brithCounter)
 app.use("/login", auth)
 app.use("/forIndex", forIndex)
 
-nodeSchedule.scheduleJob('0 0 1 * *', () => {
-  let str = ""
-  getName().then(info => {
-    if (info.length === 0) return str = "No brithday for this month", sendMail(str)
-    info.forEach(Element => {
-      str += `<li>${Element.name} day is ${Element.baseTime}</li>\n`
-    })
-    let template = `
-    <p>This birthday list</p>
-    <ul>${str}</ul>
-    `
-    return sendMail(template)
-  })
-})
-
-nodeSchedule.scheduleJob('0 0 * * *', () => {
-  let str = ""
-  getTodayName().then(info => {
-    if (info.length === 0) return
-    info.forEach(Element => {
-      str += `<li>${Element.name} bday</li>\n`
-    })
-    let template = `
-    <p>Today birthday list and wishes them...ps</p>
-    <ul>${str}</ul>
-    `
-    return sendMail(template)
-  })
-})
-
-nodeSchedule.scheduleJob('*/10 * * * *', () => {
-  return callMe()
-})
 
 app.get('/', (req, res) => {
   res.send('homes page');
@@ -76,6 +43,59 @@ mongoose.connect(
   process.env.DB_CONNECT, {
   useUnifiedTopology: true,
   useNewUrlParser: true
-}, () => console.log('connect to the db'));
+}, () => console.log(`connect to the db`));
 
-app.listen(port, () => console.log('server in running'))
+
+
+if (cluster.isMaster) {
+  console.log(`master node id ${process.pid}`)
+  nodeSchedule.scheduleJob('0 0 1 * *', () => {
+    let str = ""
+    getName().then(info => {
+      if (info.length === 0) return str = "No brithday for this month", sendMail(str)
+      info.forEach(Element => {
+        str += `<li>${Element.name} day is ${Element.baseTime}</li>\n`
+      })
+      let template = `
+      <p>This birthday list</p>
+      <ul>${str}</ul>
+      `
+      return sendMail(template)
+    })
+  })
+
+  nodeSchedule.scheduleJob('0 0 * * *', () => {
+    let str = ""
+    getTodayName().then(info => {
+      if (info.length === 0) return
+      info.forEach(Element => {
+        str += `<li>${Element.name} bday</li>\n`
+      })
+      let template = `
+      <p>Today birthday list and wishes them...ps</p>
+      <ul>${str}</ul>
+      `
+      return sendMail(template)
+    })
+  })
+
+  nodeSchedule.scheduleJob(' */10 * * * *', () => {
+    return callMe()
+  })
+
+
+  for (let i = 0; i < numCpu; i++) {
+    cluster.fork()
+  }
+  cluster.on('online', function (worker) {
+    console.log('Worker ' + worker.process.pid + ' is online');
+  });
+
+  cluster.on('exit', function (worker, code, signal) {
+    console.log('Worker ' + worker.process.pid + ' died with code: ' + code + ', and signal: ' + signal);
+    console.log('Starting a new worker');
+    cluster.fork();
+  });
+} else {
+  app.listen(port, () => console.log(`server in running`))
+}
